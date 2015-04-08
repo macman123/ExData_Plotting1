249c6d1a04d13ef6bdd5d671dfa48f4435fd622f
macman123 <mislav.acman@gmail.com>
2015-04-08 23:30:51 +0200

    commit 1

---
+++ plot2.R
@@ -0,0 +1,24 @@
+
+## reading the data into data frame format
+
+setwd("D:/Mislav/Data Science spec/4_ExploratoryDataAnalysis/ExData_Plotting1")
+library(data.table)
+data <- fread("household_power_consumption.txt", header=TRUE, sep=";", 
+              na.strings='?', data.table=FALSE)
+
+
+##extracting specific dates, converting date into POSIXlt and data into numeric
+
+data <- data[data$"Date" %in% c("1/2/2007","2/2/2007"), ]
+data["Date"] <- paste(data$"Date", data$"Time", sep=" ")
+data <- data[,c(1,3,4,5,6,7,8,9)]
+data$"Date" <- strptime(data$"Date", format="%d/%m/%Y %H:%M:%S")
+data[,2:8]<-sapply(data[,2:8],as.numeric)
+
+
+##plotting
+
+png("plot2.png")
+with(data, plot(Date, Global_active_power, type="l", xlab=NA,
+                ylab="Global Active Power (kilowatts)"))
+dev.off()
